fa2c6bf72063df3881df8da4b8ab104fae26adcb
amfl <amfl@none.none>
2015-07-10 07:59:29 +0000

    Added htop to software list

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,7 +23,8 @@ function git-update() {
 }
 function doIt() {
 	echo Installing a bunch of software...
-	sudo apt-get install build-essential cmake zsh tig silversearcher-ag ncdu
+	sudo apt-get update
+	sudo apt-get install build-essential cmake zsh tig silversearcher-ag ncdu htop
 
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "notes.md" -av --no-perms . ~
