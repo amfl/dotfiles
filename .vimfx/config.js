28c025200f577febf30e3235e1314addd811944a
amfl <amfl@none.none>
2016-05-08 19:29:50 +1200

    Add .vimfx for Firefox

---
+++ .vimfx/config.js
@@ -0,0 +1,8 @@
+// To make FF look at this file, set in about:config:
+//   extensions.VimFx.config_file_directory = path/to/.vimfx
+
+// To reload config:  zr
+
+vimfx.set('mode.normal.history_back', '<c-o>')
+vimfx.set('mode.normal.history_forward', '<c-i>')
+vimfx.set('mode.normal.follow_in_tab', 'c')
