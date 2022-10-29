e466f0f8c4bc6505552d6d6548895f347cf0c1cf
amfl <amfl@none.none>
2022-10-29 16:02:17 +1300

    emacs: Add package system

--- conf/.emacs.d/init.el
+++ conf/.emacs.d/init.el
@@ -1,6 +1,27 @@
+;; Inspiration: https://github.com/daviwil/emacs-from-scratch/blob/master/Emacs.org
+
+;; PACKAGE SYSTEM
+
+;; `package` is an emacs built-in.
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;; Define our package sources.
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
+
+(unless package-archive-contents  ;; Populate package cache if it's empty.
+  (package-refresh-contents))     ;; (Will this work with a proxy?)
+
+;; Confusingly, we use a third-party package called "use-package" to do
+;; our package management, rather than the emacs built-in "package".
+;; ...Let's fetch "use-package" by using "package"!
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)  ;; Fetch packages dynamically if required
+
 ; (custom-set-variables
 ;  ;; custom-set-variables was added by Custom.
 ;  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,8 +46,8 @@
 (setq evil-want-keybinding nil) ; Required for evil-collection
 (evil-mode 1)
 
-; Astonishingly, evil doesn't just... work. It's half broken for several emacs
-; modes (Eg, "Enter" doesn't work to follow links when viewing the manual).
-; This fixes a bunch of stuff.
-(when (require 'evil-collection nil t)
-  (evil-collection-init))
+; ; Astonishingly, evil doesn't just... work. It's half broken for several emacs
+; ; modes (Eg, "Enter" doesn't work to follow links when viewing the manual).
+; ; This fixes a bunch of stuff.
+; (when (require 'evil-collection nil t)
+;   (evil-collection-init))
