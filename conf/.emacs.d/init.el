8bcb1077b5bb222fc83e49dd43db5defe8d03195
amfl <amfl@none.none>
2022-10-29 16:34:50 +1300

    emacs: Add theme stub

--- conf/.emacs.d/init.el
+++ conf/.emacs.d/init.el
@@ -35,6 +35,8 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+; (load-theme 'wombat)
+
 ; (custom-set-variables
 ;  ;; custom-set-variables was added by Custom.
 ;  ;; If you edit it by hand, you could mess it up, so be careful.
