ddc3b4c9f454f4dfe4c970e518ddcdca35e49f5a
amfl <amfl@none.none>
2023-09-10 12:42:11 +1200

    nvim: gitsigns: Update config syntax

--- conf/.config/nvim/init.lua
+++ conf/.config/nvim/init.lua
@@ -246,36 +246,46 @@ require("packer").startup(function()
                 update_debounce = 300, -- Increase to prevent flickering
 
                 -- This whole mess is the default keybinds from the readme.
-                -- The syntax can change on nvim 0.7+ to use `vim.keymap.set`
                 on_attach = function(bufnr)
-                    local function map(mode, lhs, rhs, opts)
-                        opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
-                        vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
-                    end
+                  local gs = package.loaded.gitsigns
 
-                    -- Navigation
-                    map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
-                    map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
+                  local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                  end
 
-                    -- Actions
-                    map('n', '<leader>hs', ':Gitsigns stage_hunk<CR>')
-                    map('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
-                    map('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
-                    map('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
-                    map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
-                    map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
-                    map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
-                    map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
-                    map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
-                    map('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
-                    map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
-                    map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
-                    map('n', '<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
+                  -- Navigation
+                  map('n', ']c', function()
+                    if vim.wo.diff then return ']c' end
+                    vim.schedule(function() gs.next_hunk() end)
+                    return '<Ignore>'
+                  end, {expr=true})
 
-                    -- Text object
-                    map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-                    map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-                end,
+                  map('n', '[c', function()
+                    if vim.wo.diff then return '[c' end
+                    vim.schedule(function() gs.prev_hunk() end)
+                    return '<Ignore>'
+                  end, {expr=true})
+
+                  -- Actions
+                  map('n', '<leader>hs', gs.stage_hunk)
+                  map('n', '<leader>hr', gs.reset_hunk)
+                  map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+                  map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+                  map('n', '<leader>hS', gs.stage_buffer)
+                  map('n', '<leader>hu', gs.undo_stage_hunk)
+                  map('n', '<leader>hR', gs.reset_buffer)
+                  map('n', '<leader>hp', gs.preview_hunk)
+                  map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+                  map('n', '<leader>tb', gs.toggle_current_line_blame)
+                  map('n', '<leader>hd', gs.diffthis)
+                  map('n', '<leader>hD', function() gs.diffthis('~') end)
+                  map('n', '<leader>td', gs.toggle_deleted)
+
+                  -- Text object
+                  map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+                end
             }
         end }
 
